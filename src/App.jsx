0faf4fb54b1b2a3ff4d8895d552a797b5d1402fd
Peyton <peytonmccutcheon4@gmail.com>
2024-04-30 15:53:13 -0700

    Starting point, no css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import './App.css'
+import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
+import InputArea from "./InputArea";
 
 function App() {
-  return (
-    <h1 className="text-4xl text-red-500 font-bold underline">
-      Hello world!!!!!
-    </h1>
-  )
+	const [items, setItems] = useState([]);
+
+	function addItem(inputText) {
+		setItems((prevItems) => {
+			return [...prevItems, inputText];
+		});
+	}
+
+	function deleteItem(id) {
+		setItems((prevItems) => {
+			return prevItems.filter((item, index) => {
+				return index !== id;
+			});
+		});
+	}
+
+	return (
+		<div className="container">
+			<div className="heading">
+				<h1>To-Do List</h1>
+			</div>
+			<InputArea onAdd={addItem} />
+			<div>
+				<ul>
+					{items.map((todoItem, index) => (
+						<ToDoItem key={index} id={index} text={todoItem} onChecked={deleteItem} />
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
